b2e62817981aff7f00de9f46a5bdb3e371382ad3
Grigory Aksentyev <grigory.aksentiev@gmail.com>
2016-10-14 15:34:13 +0300

    travis ci

--- mock/hubble/hubble_mock.go
+++ mock/hubble/hubble_mock.go
@@ -1,24 +1,32 @@
 package hubble_mock
 
-import "github.com/aksentyev/hubble/hubble"
+import (
+    "github.com/aksentyev/hubble/hubble"
+    "fmt"
+)
 
 // Mock of BackendAdapter interface
 type MockBackendAdapter struct {}
 type MockKVBackendAdapter struct {}
 
 func NewMockBackendAdapter() *MockBackendAdapter {
-	mock := MockBackendAdapter{}
-	return &mock
+    mock := MockBackendAdapter{}
+    return &mock
 }
 
 func NewMockKVBackendAdapter() *MockKVBackendAdapter {
-	mock := MockKVBackendAdapter{}
-	return &mock
+    mock := MockKVBackendAdapter{}
+    return &mock
 }
 
 func (m *MockBackendAdapter) GetAll() (list []*hubble.Service, err error) {
     for i := 0; i < 2; i++ {
         svc := hubble.DefaultService()
+        svc.Name = fmt.Sprintf("testsvc%v", string(i))
+        svc.Addresses = map[string]string{"srv1": fmt.Sprintf("10.0.0.1%v", string(i))}
+        svc.Port = "9999"
+        svc.Tags = []string{"test"}
+        svc.ServiceParams.ModifyIndex = uint64(123456789) + uint64(i)
         list = append(list, svc)
     }
     return list, err
