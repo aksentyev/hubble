a844cbbdde90a1819d3cd776a202b7a0b1583048
Grigory Aksentyev <grigory.aksentiev@gmail.com>
2016-10-14 16:02:04 +0300

    add testing script

---
+++ testandcover.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+function die() {
+  echo $*
+  exit 1
+}
+
+# Initialize profile.cov
+echo "mode: count" > profile.cov
+
+# Initialize error tracking
+ERROR=""
+
+# Test each package and append coverage profile info to profile.cov
+for pkg in `cat .testpackages`
+do
+    #$HOME/gopath/bin/
+    go test -v -covermode=count -coverprofile=profile_tmp.cov $pkg || ERROR="Error testing $pkg"
+    tail -n +2 profile_tmp.cov >> profile.cov || die "Unable to append coverage for $pkg"
+done
+
+if [ ! -z "$ERROR" ]
+then
+    die "Encountered error, last error was: $ERROR"
+fi
